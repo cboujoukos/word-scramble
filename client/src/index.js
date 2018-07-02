e3cc80901e35db5c23e44bee46466c369bec8ead
Chris Boujoukos <github email address>
2018-07-02 13:53:58 -0400

    added comments

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+
+// REMOVE THIS LINE IF SETTING UP ACTIVE ADMIN
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+// REMOVE THIS LINE IF SETTING UP ACTIVE ADMIN
 registerServiceWorker();
