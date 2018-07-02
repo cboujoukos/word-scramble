e42b6ba86fc19ed58d985cd42a16722f4cc1e194
Chris Boujoukos <github email address>
2018-07-02 15:59:13 -0400

    add more verbose json response

--- rails-api/app/controllers/words_controller.rb
+++ rails-api/app/controllers/words_controller.rb
@@ -1,11 +1,13 @@
 class WordsController < ApplicationController
   def index
-    @words = Word.all
-    render json: @words.to_json
+    words = Word.all
+    render json: {status: 'SUCCESS', message: 'Loaded all words', data: words}, status: :ok
+    # render json: words.to_json
   end
 
   def show
-    @word = Word.find(params[:id])
-    render json: @word.to_json
+    word = Word.find(params[:id])
+    # render json: word.to_json
+    render json: {status: 'SUCCESS', message: 'Loaded word', data: word}, status: :ok
   end
 end
