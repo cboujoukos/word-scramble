942bb676b3e973722297fffeac5898994287623f
Chris Boujoukos <github email address>
2018-07-02 15:18:29 -0400

    add show action

--- rails-api/app/controllers/words_controller.rb
+++ rails-api/app/controllers/words_controller.rb
@@ -3,4 +3,9 @@ class WordsController < ApplicationController
     @words = Word.all
     render json: @words.to_json
   end
+
+  def show
+    @word = Word.find(params[:id])
+    render json: @word.to_json
+  end
 end
