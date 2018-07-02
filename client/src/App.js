80a6713c31ca97de908d399cd26ef4283266ccb3
Chris Boujoukos <github email address>
2018-07-02 15:43:40 -0400

    add fetch to test connection on componentDidMount

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,15 @@ import Game from './components/Game';
 import './App.css';
 
 class App extends Component {
+
+  // TESTING CONNECTION TO API
+  // componentDidMount() {
+  //   window.fetch('api/words')
+  //     .then(rsp => rsp.json())
+  //     .then(json=> console.log(json))
+  //     .catch(error=> console.log(error))
+  // }
+
   render() {
     return (
       <div className="App">
