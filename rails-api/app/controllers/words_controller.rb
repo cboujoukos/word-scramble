4a004a0cbc7e8bb21337acc30e38fd2d61413af1
Chris Boujoukos <github email address>
2018-07-03 10:22:44 -0400

    add scrambled word function to json rsp

--- rails-api/app/controllers/words_controller.rb
+++ rails-api/app/controllers/words_controller.rb
@@ -5,7 +5,12 @@ class WordsController < ApplicationController
     else
       words = Word.all
     end
-    render json: {status: 'SUCCESS', message: 'Loaded data', data: word || words}, status: :ok
+    render json: {
+      status: 'SUCCESS',
+      message: 'Loaded data',
+      data: word || words,
+      scramble: word.name.split('').shuffle.join() || undefined
+      }, status: :ok
     # render json: words.to_json
   end
 
