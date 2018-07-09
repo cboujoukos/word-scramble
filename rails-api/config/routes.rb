c9cd69ac5b7c3f8815f685300c4a1e071504a4c7
Chris Boujoukos <github email address>
2018-07-09 11:51:26 -0400

    add route for high_scores to api

--- rails-api/config/routes.rb
+++ rails-api/config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   scope '/api' do
-    resources :words
+    resources :words, only: [:index, :show]
+    resources :high_scores, only: [:index]
   end
 end
