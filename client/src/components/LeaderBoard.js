078493f781bf1223c16f15ea5d71cfd9074a0d75
Chris Boujoukos <github email address>
2018-07-09 11:32:01 -0400

    style leaderboard table

--- client/src/components/LeaderBoard.js
+++ client/src/components/LeaderBoard.js
@@ -3,7 +3,34 @@ import React, { Component } from 'react';
 export default class LeaderBoard extends Component {
   render(){
     return(
-      <h1>High Scores</h1>
+      <div>
+        <h1>High Scores</h1>
+
+        <table className="score-table">
+          <tbody>
+            <tr>
+              <th>Rank</th>
+              <th>Score</th>
+              <th>Name</th>
+            </tr>
+            <tr>
+              <td>1st</td>
+              <td>20000</td>
+              <td>???</td>
+            </tr>
+            <tr>
+              <td>2nd</td>
+              <td>18000</td>
+              <td>???</td>
+            </tr>
+            <tr>
+              <td>3rd</td>
+              <td>15000</td>
+              <td>???</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     )
   }
 }
